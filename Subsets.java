9342f6de9b4445d397fe2f2f1d8c3de2b91547ba
Qinglin,Xia <xiaqinglinv5@gmail.com>
2015-09-18 19:09:01 -0700

    Create Subsets.java

---
+++ Subsets.java
@@ -0,0 +1,33 @@
+There are two ways to solve the problem
+1. Recursive (DFS + Backtracking)
+2. Iterative 
+
+1. 
+<
+class Solution {
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public ArrayList<ArrayList<Integer>> subsets(ArrayList<Integer> S) {
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> subsets = new ArrayList<ArrayList<Integer>>();
+        Collections.sort(S);
+        subsets.add(temp);
+        helper(subsets,S,temp,0);
+        return subsets;
+        
+    }
+    public void helper(ArrayList<ArrayList<Integer>> subsets, ArrayList<Integer> S, ArrayList<Integer> temp, int index) {
+        
+        for (int i = index; i< S.size(); i++) {
+            temp.add(S.get(i));
+            subsets.add(new ArrayList<Integer>(temp));
+            helper(subsets,S,temp,i+1);
+            temp.remove(temp.size()-1);
+        }
+        
+    }
+}
+
+>
